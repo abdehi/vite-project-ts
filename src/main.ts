19377fff01ec6da4cfa75e48a44f5bc0576a282d
Abdehi Perdana Ain <abdehiperdana@gmail.comabdehiperdana@gmail.com>
2023-02-12 10:35:03 +0800

    three com

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import "./style.css";
 // import typescriptLogo from './typescript.svg'
-import { setupCounter } from "./counter";
+// import { setupCounter } from "./counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <h1> hello world </h1>
 `;
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
